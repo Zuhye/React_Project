33f1a27f914a521284eb4e7374e06bc63a261761
JuHye Song <mari5669@naver.com>
2021-11-21 02:02:27 +0900

    Add Material UI Design for Add/Delete

--- client/src/App.js
+++ client/src/App.js
@@ -79,6 +79,7 @@ class App extends Component {
               <TableCell>생년월일</TableCell>
               <TableCell>성별</TableCell>
               <TableCell>직업</TableCell>
+              <TableCell>설정</TableCell>
             </TableRow>
 
           </TableHead> 
